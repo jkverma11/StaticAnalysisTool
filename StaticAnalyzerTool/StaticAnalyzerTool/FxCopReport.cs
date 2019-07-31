d1f52ad5621ecbcf93145a02357462175d158052
Jitendra.Kumar <jitendraverma1497@gmail.com>
2019-07-31 10:46:00 +0530

    Adding Report for NDepend and FxCop

---
+++ StaticAnalyzerTool/StaticAnalyzerTool/FxCopReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace StaticAnalyzerTool
+{
+    class FxCopReport
+    {
+        public static void Main(string[] args)
+        {
+            string Fxcop_xml_out = "C:\\Users\\320050491\\Downloads\\FxCopOutput\\FxCopReport.xml";
+
+            var xmlDoc = XElement.Load(Fxcop_xml_out);
+
+            var slnPath = (from element in xmlDoc.DescendantsAndSelf()
+                where element.Name == "Target"
+                select element).FirstOrDefault();
+
+            Console.WriteLine("the solution path is: " + slnPath.Attribute("Name").Value);
+        }
+    }
+}
---
+++ StaticAnalyzerTool/StaticAnalyzerTool/NDependXmlReport.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace StaticAnalyzerTool
+{
+    class NDependXmlReport
+    {
+        public static void Main(string[] args)
+        {
+            string NDependXmloutput = "C:\\Users\\320050491\\Downloads\\NDependOutput\\TrendMetrics\\NDependTrendData2019.xml";
+            XmlTextReader reader = new XmlTextReader(NDependXmloutput);
+            Dictionary<string, string > NDependMetrics = new Dictionary<string, string>();
+            List<string> AttributeName = new List<string>();
+            string[] AttribueValue = null;
+            
+            int flag = 0;
+
+            while (reader.Read() && flag == 0)
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        if(reader.Name == "Metric")
+                        {
+                            while (reader.MoveToNextAttribute())
+                            {
+                                if (reader.Name == "Name")
+                                {
+                                    AttributeName.Add(reader.Value);
+                                }
+                            }
+                            
+                        }
+                        if (reader.Name == "R" && flag == 0)
+                        {
+                            int i = 0;
+                            while (reader.MoveToNextAttribute()) // Read the attributes.
+                            {
+                                if (i == 2 && reader.Name == "V")
+                                {
+                                    
+                                    string ans = reader.Value;
+                                    AttribueValue = ans.Split('|');
+                                }
+                                i++;
+                            }
+                            flag = 1;
+                        }
+                        break;
+
+                    case XmlNodeType.Text:
+                        break;
+                    case XmlNodeType.EndElement:
+                        break;
+                }
+            }
+
+            for (int k = 0; k <AttributeName.Count; k++)
+            {
+                NDependMetrics.Add(AttributeName[k], AttribueValue[k]);
+            }
+
+            foreach (var item in NDependMetrics)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
